611107c50e46efb76fddf9cdadca28cd630b1a55
zsluss <133190917+zsluss@users.noreply.github.com>
2023-08-24 20:55:45 -0500

    updates

--- components/translator.js
+++ components/translator.js
@@ -5,6 +5,52 @@ const britishOnly = require('./british-only.js')
 
 class Translator {
 
+    amtobr(text) {
+        let original = text
+        for (let i = 0; i < 183; i++) {
+            let regex = new RegExp(Object.keys(americanOnly)[i] , 'i')  //Object.values for the side
+            if (original.search(regex) !== -1) {
+                let adjustment = Object.values(americanOnly)[i]
+                if (original[original.search(regex)] === original[original.search(regex)].toUpperCase()) {adjustment = adjustment[0].toUpperCase() + adjustment.slice(1) }
+                original = original.replace(regex, "<span class=\"highlight\">" + adjustment + "</span>")
+                i = i - 1
+                console.log(1)
+            }
+        }
+        for (let k = 0; k < 1699; k++) {
+            let regex = new RegExp(Object.keys(americanToBritishSpelling)[k], 'i')  //Object.values for the side
+            if (original.search(regex) !== -1) {
+                let adjustment = Object.values(americanToBritishSpelling)[k]
+                if (original[original.search(regex)] === original[original.search(regex)].toUpperCase()) {adjustment = adjustment[0].toUpperCase() + adjustment.slice(1) }
+                original = original.replace(regex, "<span class=\"highlight\">" + adjustment + "</span>")
+                k = k - 1
+                console.log(2)
+            }
+        }
+        for (let j = 0; j < 6; j++) {
+            let regex = new RegExp(Object.keys(americanToBritishTitles)[j], 'i')  //Object.values for the side
+            if (original.search(regex) !== -1) {
+                let adjustment = Object.values(americanToBritishTitles)[j]
+                if (original[original.search(regex)] === original[original.search(regex)].toUpperCase()) {adjustment = adjustment[0].toUpperCase() + adjustment.slice(1) }
+                original = original.replace(regex, "<span class=\"highlight\">" + adjustment + "</span>")
+                console.log(3)
+            }
+        }
+        let regexTime = /\d:\d/i
+        if (original.search(regexTime) !== -1){
+            console.log(original.search(regexTime))
+            console.log(original[original.search(regexTime)])
+          original = original.slice(0,original[original.search(regexTime)]) + '<span class=\"highlight\">.</span>' + original.slice(original[original.search(regexTime) + 3] ) 
+        }
+          
+        return original
+    }
+
+    brtoam(text) {
+
+
+    }
+
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,34 @@
 const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
-  
+
   const translator = new Translator();
+
 
   app.route('/api/translate')
     .post((req, res) => {
-      
-    });
-};
+      let text = req.body.text;
+      let locale = req.body.locale;
+
+      if (text === undefined || locale == undefined) { return res.json({ error: 'Required field(s) missing' }) }
+      if (text === "") { return res.json({ error: 'No text to translate' }) }
+      if (locale !== 'british-to-american') { if (locale !== 'american-to-british') { return res.json({ error: 'Invalid value for locale field' }) } }
+      if (locale === 'american-to-british') {
+        let answer = translator.amtobr(text)
+        console.log(answer)
+        res.json({
+          text: text,
+          translation: answer
+        })
+      }
+      if (locale === 'british-to-american') {
+        translator.brtoam(text)
+      }
+
+      let valid
+      if (valid === 'valid') { res.send("Everything looks good to me!") }
+
+
+
+    })
+}
